e4d1907707ada392e8022f930bdc677645520c19
Oscar I. Zari <156673803+oiz05@users.noreply.github.com>
2026-05-24 16:00:30 -0500

    Update apiClient base URL to production endpoint

--- src/shared/apiClient.ts
+++ src/shared/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "/api",
+    baseURL: "https://cj-196a962e499340109b9631b8eef0bb37.ecs.sa-east-1.on.aws/api",
     headers: {
         "Content-Type": "application/json",
     },
