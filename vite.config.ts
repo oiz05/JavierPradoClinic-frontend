6936b73f3457b3844e1e1dac19817c5b779d99de
oiz05 <isaac.zari60@gmail.com>
2026-06-07 15:11:33 -0500

    ptmre >:v

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => {
         '/api': {
           target: env.VITE_API_TARGET,
           changeOrigin: true,
-          rewrite: (path) => path.replace(/^\api/,''),
+          rewrite: (path) => path.replace(/^\/api/,''),
         },
       },
     },
