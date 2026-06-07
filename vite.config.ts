89c01dd4f24abf030fc1efeb3ade5903ef4d33d9
oiz05 <isaac.zari60@gmail.com>
2026-06-07 14:56:04 -0500

    si no funciona me pego un tiro

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
     plugins: [react(), tailwindcss()],
     server: {
       proxy: {  
-        '/': {
+        '/api': {
           target: env.VITE_API_TARGET,
           changeOrigin: true,
         },
